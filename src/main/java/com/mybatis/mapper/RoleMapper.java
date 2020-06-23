a355e36b9d1e74ad16c85af682cf775072f89b6e
yindongzi <yindongzi@163.com>
2020-06-23 19:23:42 +0800

    init mysqltest

---
+++ src/main/java/com/mybatis/mapper/RoleMapper.java
@@ -0,0 +1,5 @@
+package com.mybatis.mapper;
+import com.mybatis.po.Role;
+public interface RoleMapper {
+    public Role getRole(Long id);
+}
---
+++ src/main/java/com/mybatis/po/Role.java
@@ -0,0 +1,42 @@
+package com.mybatis.po;
+
+public class Role {
+    private Long id;
+    private String roleName;
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", roleName='" + roleName + '\'' +
+                ", note='" + note + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    private String note;
+/**setter and getter**/
+}
