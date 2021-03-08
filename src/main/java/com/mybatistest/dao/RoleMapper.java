3a954883edfb1def1ca8c9111605cd38d8c9bee2
yindo <yindongzi@163.com>
2021-03-08 13:01:38 +0800

    修改配置和sqlSessionFactory使用方式

--- src/main/java/com/mybatistest/dao/RoleMapper.java
+++ src/main/java/com/mybatistest/dao/RoleMapper.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.annotations.Select;
 
 public interface RoleMapper {
     public Role getRole(Integer id);
-    @Select(value = "select id ,role_name as roleName ,note from role where id = #{id}")
+    String str = "select id ,role_name as roleName ,note from role where id = #{id}";
+    @Select(value = str)
     public Role getRole1(int id);
 }
