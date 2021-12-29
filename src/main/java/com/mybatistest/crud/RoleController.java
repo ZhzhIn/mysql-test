f88e1eca20e2832ffbd553cb3d9efca50433e1bd
yindo <yinzhenzhi@tenwit.com>
2021-12-29 10:51:42 +0800

    test Code

--- src/main/java/com/mybatistest/crud/RoleController.java
+++ src/main/java/com/mybatistest/crud/RoleController.java
@@ -10,10 +10,7 @@ import org.junit.jupiter.api.Test;
 @Slf4j
 public class RoleController {
     SqlSession sqlSession = null;
-//    SqlSession sqlSession = null;
-//    SqlSession sqlSession = null;
-//    hotfix
-//    hotfix
+//    test code
     public Role selectRole(int id) {
         try {
             sqlSession = SqlSessionFactoryUtil.openSqlSession();
