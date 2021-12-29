5af9b845aab14fcb0e88dd50f2d6eb0cf2b64b31
yindo <yinzhenzhi@tenwit.com>
2021-12-29 10:40:51 +0800

    修改配置和sqlSessionFactory使用方式

--- src/main/java/com/mybatistest/crud/RoleController.java
+++ src/main/java/com/mybatistest/crud/RoleController.java
@@ -10,7 +10,10 @@ import org.junit.jupiter.api.Test;
 @Slf4j
 public class RoleController {
     SqlSession sqlSession = null;
-
+//    SqlSession sqlSession = null;
+//    SqlSession sqlSession = null;
+//    hotfix
+//    hotfix
     public Role selectRole(int id) {
         try {
             sqlSession = SqlSessionFactoryUtil.openSqlSession();
